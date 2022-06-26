47ad372373509f76b4bbca9429d19a52c0bb9d8b
noejasiel <55604384+noejasiel@users.noreply.github.com>
2022-06-25 19:18:30 -0500

    se mod la respuesta

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send('<h1>HOLA</h1>');
+  res.send("<h1>HOLA MUNDO</h1>");
 });
 
 const PORT = process.env.PORT || 5000;
@@ -10,4 +10,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`escuchando en el puerto ${PORT}`);
 });
- 
