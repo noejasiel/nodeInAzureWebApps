8a7689142c460a619ccd788c10d0e79f57af8857
noejasiel <55604384+noejasiel@users.noreply.github.com>
2022-06-25 19:30:45 -0500

    se aadio nodemos y gitignore

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("<h1>HOLA MUNDO</h1>");
+  res.send("<h1>HOLA MUNDO new</h1>");
 });
 
 const PORT = process.env.PORT || 5000;
