d0edd13655bba2c50a04c0bc20ab8dffb8fd10c0
noejasiel <55604384+noejasiel@users.noreply.github.com>
2022-06-28 02:24:51 -0500

    se subiwron cambios a la web y ya jala

--- index.js
+++ index.js
@@ -4,43 +4,48 @@ require("dotenv").config();
 const path = require("path");
 const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
-// mongoose
-//   .connect(
-//     `mongodb+srv://jasiel:${process.env.PASSWORD_BBDD}@development.eu72k.mongodb.net/firstDBMongo?retryWrites=true&w=majority`
-//   )
-//   .then((result) => {
-//     app.listen(PORT, () => {
-//       console.log(`escuchando en el puerto ${PORT}`);
-//     });
-//   })
-//   .catch((error) => console.log(error));
+mongoose
+  .connect(
+    `mongodb+srv://jasiel:${process.env.PASSWORD_BBDD}@development.eu72k.mongodb.net/firstDBMongo?retryWrites=true&w=majority`
+  )
+  .then((result) => {
+    console.log(`me conecte`);
+  })
+  .catch((error) => console.log(error));
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 
-// const productSchema = mongoose.Schema(
-//   {
-//     curseValue: { type: String, required: true },
-//     priceValue: Number,
-//   },
-//   {
-//     timestamps: true,
-//   }
-// );
+const productSchema = mongoose.Schema(
+  {
+    curseValue: { type: String, required: true },
+    priceValue: Number,
+  },
+  {
+    timestamps: true,
+  }
+);
 
-// const Product = mongoose.model("Product", productSchema);
+const Product = mongoose.model("Product", productSchema);
 
-// app.post("/api/v1/curses", (req, res) => {
-//   console.log({ body: req.body });
-//   const newProduct = new Product(req.body);
-//   newProduct
-//     .save()
-//     .then(() => {
-//       res.status(201).json({ ok: true });
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//     });
-// });
+app.post("/api/v1/curses", async (req, res) => {
+  // console.log({ body: req.body });
+  const newProduct = await new Product(req.body);
+
+  await newProduct
+    .save()
+    .then((response) => {
+      res.status(201).json({ ok: true });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  // console.log(req.body);
+  // res.status(201).send("aqui yea");
+});
+
+app.listen(PORT, () => {
+  console.log(`escuchando en el puerto ${PORT}`);
+});
--- public/inde.js
+++ public/inde.js
@@ -3,22 +3,20 @@ const curse = document.getElementById("inputCurso");
 const price = document.getElementById("inputPrice");
 
 console.log(btnSubmit);
-// console.log(e.target);
 
 btnSubmit.addEventListener("click", (e) => {
   e.preventDefault();
   const curseValue = curse.value;
   const priceValue = price.value;
   console.log(curseValue, priceValue);
-
-  // fetch("/api/v1/curses", {
-  //   method: "POST",
-  //   headers: {
-  //     "Content-Type": "application/json",
-  //   },
-  //   body: JSON.stringify({
-  //     curseValue,
-  //     priceValue,
-  //   }),
-  // });
+  fetch("/api/v1/curses", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      curseValue,
+      priceValue,
+    }),
+  });
 });
